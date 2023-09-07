d1a03e1ac9f9c1d20b3942df04b87e56e8c5d647
Kai Lin <kevin_lin@alumni.brown.edu>
2023-09-07 19:39:18 -0400

    first

--- hash-table.js
+++ hash-table.js
@@ -12,14 +12,25 @@ class HashTable {
 
   constructor(numBuckets = 4) {
     // Your code here
+    this.count = 0;
+    this.capacity = numBuckets;
+    this.data = new Array(numBuckets).fill(null)
   }
 
   hash(key) {
     // Your code here
+    let hexNum = sha256(key);
+    let shortHex = hexNum.slice(0, 8);
+    // console.log(shortHex);
+    let answer = parseInt(shortHex, 16);
+    return answer
   }
 
   hashMod(key) {
     // Your code here
+    let hash = this.hash(key);
+    let answer = hash % this.capacity;
+    return answer;
   }
 
   insertNoCollisions(key, value) {
